74e87900d70ca5e9abed15c769c5bcd0cbd04f69
Trenden Leonard <leonard.trenden@gmail.com>
2022-12-18 23:41:20 -0600

    Set up for login authentication

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from leads.views import landing_page, LandingPageView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", LoginView.as_views(), name="login" ),
     path("", LandingPageView.as_view(), name="landing_page"),
     path("leads/", include("leads.urls", namespace="leads")),
 ]
